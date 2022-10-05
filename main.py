6d4f2f7e4778669d9ee444dd94f5cbd5e52b025a
Simon <simon@simonrijntjes.nl>
2022-10-05 10:44:53 +0200

    A much better styling of the newspaper!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+
 import bs4
 import requests
-import json
 
 
 def get_posts():
@@ -53,8 +54,7 @@ def main():
                 thumbnail = posts[i]['placeable']['leadComponent']['standard']['url']
                 description = posts[i]['placeable']['dek']['html']
             except:
-                thumbnail = None
-                description = None
+                continue
             data.append({'title': posts[i]['placeable']['title'],
                          'description': description,
                          'author': posts[i]['placeable']['author'],
@@ -72,10 +72,10 @@ def main():
         f.close()
 
     # Send the newspaper to the user
-    send_newspaper()
+    send_newspaper(numbers)
 
 
-def send_newspaper():
+def send_newspaper(numbers):
     """ Send the newspaper to the user """
     # Get the posts
     print('Sending newspaper')
@@ -83,19 +83,30 @@ def send_newspaper():
         data = json.load(f)
         # Set up an HTML file to send to the user
         with open('newspaper.html', 'w', encoding='utf-8') as f:
-            f.write('<html><head><title>News</title></head><body>')
-            # Give the page some CSS
             f.write(
-                '<style>body {background-color: #000000; color: #ffffff; font-family: Arial, Helvetica, sans-serif; '
-                'font-size: 20px;}</style>')
+                '<!DOCTYPE html> <html lang="en"> <head> <title>The Tech Morning Newspaper</title> <link '
+                'href="https://fonts.gstatic.com" rel="preconnect"/> <link '
+                'href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" '
+                'rel="stylesheet"/> <style> body { margin: 0; } td, p { font-size: 13px; color: #878787; } ul { '
+                'margin: 0 0 10px 25px; padding: 0; } li { margin: 0 0 3px 0; } h1, h2 { color: black; } h1 { '
+                'font-size: 25px; } h2 { font-size: 20px; } a { color: #2F82DE; font-weight: bold; text-decoration: '
+                'none; } .entire-page { background: #C7C7C7; width: 100%; padding: 20px 0; font-family: Lucida '
+                'Grande, Lucida Sans Unicode, Verdana, sans-serif; line-height: 1.5; } .email-body { max-width: '
+                '600px; min-width: 320px; margin: 0 auto; background: white; border-collapse: collapse; } img { '
+                'max-width: 100%; } .email-header { background: black; padding: 30px; } .news-section { padding: 20px '
+                '30px; } .footer { background: #eee; padding: 10px; font-size: 10px; text-align: center; } </style> '
+                '</head> <table class="entire-page"> <tr> <td> <table class="email-body"> <tr> <td '
+                'class="email-header"> <a> <h1 style="color:white;">The Tech Morning Newspaper</h1> </a> </td> </tr> '
+                '<tr> <td class="news-section"> <h2>Good morning</h2> <p>Here are the '
+                'latest tech news from The Verge. Enjoy!</p> <ul>')
             for post in data:
-                f.write(f'<h1>{post["title"]}</h1>')
-                f.write(f'<p>By {post["author"]["fullName"]}</p>')
-                f.write(f'<p>{post["description"]}</p>')
-                f.write(f'<a href="{post["url"]}">Read more</a>')
-                if post['thumbnail'] is not None:
-                    f.write(f'<img src="{post["thumbnail"]}">')
-            f.write('</body></html>')
+                f.write(
+                    f' <tr> <td class="news-section"> <h1>{post["title"]}</h1> <a><img src="{post["thumbnail"]}"></a> '
+                    f'<p>{post["description"]}</p> <p><a href="{post["url"]}">Read Now &rarr;</a></p> </td> </tr>')
+            f.write(
+                '<tr> <td class="footer"> You are receiving this email because you subscribed to The Tech Morning '
+                'Newspaper. You can <a href="TODO: Add unsubscribe">instantly opt out</a> any time. </td> </tr> '
+                '</table> </td> </tr> </table>')
             f.close()
         print('Newspaper sent')
 
