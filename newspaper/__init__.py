baae0e028223770fcb3668abea33dd00d3990f6d
Simon <simon@simonrijntjes.nl>
2022-10-27 16:46:19 +0200

    Another fix

--- newspaper/__init__.py
+++ newspaper/__init__.py
@@ -5,18 +5,15 @@ mail = Mail()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    try:
-        app.config.from_pyfile("config.py")
-    except OSError:
-        app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-        app.config["MAIL_SERVER"] = os.environ.get('MAIL_SERVER')
-        app.config["MAIL_PORT"]: int = os.environ.get('MAIL_PORT')
-        app.config["MAIL_USE_TLS"]: bool = os.environ.get('MAIL_USE_TLS')
-        app.config["MAIL_USE_SSL"]: bool = os.environ.get('MAIL_USE_SSL')
-        app.config["MAIL_USERNAME"] = os.environ.get('MAIL_USERNAME')
-        app.config["MAIL_PASSWORD"] = os.environ.get('MAIL_PASSWORD')
-        app.config["MAIL_DEFAULT_SENDER"] = os.environ.get('MAIL_DEFAULT_SENDER')
+    app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config["MAIL_SERVER"] = os.environ.get('MAIL_SERVER')
+    app.config["MAIL_PORT"]= 465
+    app.config["MAIL_USE_TLS"] = False
+    app.config["MAIL_USE_SSL"] = True
+    app.config["MAIL_USERNAME"] = os.environ.get('MAIL_USERNAME')
+    app.config["MAIL_PASSWORD"] = os.environ.get('MAIL_PASSWORD')
+    app.config["MAIL_DEFAULT_SENDER"] = os.environ.get('MAIL_DEFAULT_SENDER')
 
     print(app.config)
 
