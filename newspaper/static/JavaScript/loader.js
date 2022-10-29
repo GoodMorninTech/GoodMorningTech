b5b646c5b600ef94752c88eb604db58755aa3f85
ImmaHarry <haritkhera@gmail.com>
2022-10-29 23:46:23 +0530

    moved all scripts to js and cleaned up

---
+++ newspaper/static/JavaScript/loader.js
@@ -0,0 +1,10 @@
+//Adding a delay of 2 seconds to fadeout the loader
+const loaderText = ["Loading Good Morning Tech", "Coming up, Good Morning Tech!", "Good Morning Tech, we're almost there!", "Loading your favorite newsletter website!"]
+//Randomizing the loader text
+const randomText = loaderText[Math.floor(Math.random() * loaderText.length)]
+//Adding the random text to the loader
+$(".loader-text").text(randomText)
+//Fading out the loader
+setTimeout(() => {
+    $(".loader-wrapper").fadeOut(1000);
+}, 1500); // 1.5 seconds 
