2e3aa8b540f37a3674a8b528226b392a1015ac58
ImmaHarry <haritkhera@gmail.com>
2022-11-09 16:15:41 +0530

    New sign up page implemented.

--- newspaper/static/JavaScript/sign-up.js
+++ newspaper/static/JavaScript/sign-up.js
@@ -1,12 +1,28 @@
+const counter = document.querySelector(".slider-counter");
 const slider = document.querySelector(".slider");
-const counter = document.querySelector(".counter");
-counter.innerHTML = `COUNT: ${slider.value} ARTICLES`;
+
+counter.innerHTML = `Count: ${slider.value} articles`;
 slider.oninput = function() {
-    counter.innerHTML = `COUNT: ${this.value} ARTICLES`;
+    counter.innerHTML = `Count: ${this.value} articles`;
 }
 
+const checkbox_input_div = document.querySelector(".days-input");
+const checkbox_input = document.querySelector(".checkbox");
+
+//Making sure that only 1 out 3 checkboxes can be selected in the document at any time
+checkbox_input_div.addEventListener("click", function(e) {
+    if (e.target.type === "checkbox") {
+        const checkboxes = document.querySelectorAll(".checkbox");
+        checkboxes.forEach(checkbox => {
+            if (checkbox !== e.target) {
+                checkbox.checked = false;
+            }
+        })
+    }
+})
+
+//Making sure that the email input is not empty
 const email = document.querySelector(".email-address");
-//Checking if email is entered correctly, if it is not, turns to #F43434, if it is left empty then default and if correct then green
 email.addEventListener("input", function(event) {
     if (email.validity.typeMismatch) {
         email.style.borderColor = "#F43434";
@@ -16,11 +32,5 @@ email.addEventListener("input", function(event) {
         email.style.borderColor = "#62A539";
     }
 });
-//Checking if form is submitted correctly, if it is not, then mark the missing fields red
-const form = document.querySelector("form");
-form.addEventListener("submit", function(event) {
-    if (!email.validity.valid) {
-        email.style.borderColor = "#F43434";
-        event.preventDefault();
-    }
-});
+
+//Checking if email already exists in the database, if it does, then the email border color will turn yellow
