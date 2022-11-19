81ed9cd2447f24e1b3c26486a5d5dd61f86c3f12
ImmaHarry <haritkhera@gmail.com>
2022-11-19 21:37:09 +0530

    Tailwind Config

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./newspaper/**/*.{html,js}"],
+    theme: {
+      fontFamily: {
+        'gmt-fira': ['Fira Sans', 'sans-serif'],
+        'gmt-karla': ['Karla', 'sans-serif'],
+      },
+      extend: {
+        colors: {
+          'gmt-red-primary': '#CF3333',
+          'gmt-red-secondary': '#D02B2B',
+          'gmt-black-primary': '#272727',
+          'gmt-yellow-primary': '#FFD037',
+          'gmt-teal-primary': '#1FC59D',
+          'gmt-gray-primary': '#646464',
+          'gmt-background': '#F6F4F0',
+          'gmt-selected-navbar': '#E6E6E6'
+        }
+      },
+    },
+    plugins: [],
+  }
