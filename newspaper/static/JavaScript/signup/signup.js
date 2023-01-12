2ffdbf7b52d8fec3ae17c447bab89338eb83763a
ImmaHarry <haritkhera@gmail.com>
2023-01-12 23:05:29 +0530

    finished, checkbox & radio need to be fixed.

--- newspaper/static/JavaScript/signup/signup.js
+++ newspaper/static/JavaScript/signup/signup.js
@@ -13,25 +13,3 @@ email.addEventListener("input", function(event) {
 const everyday = document.getElementById('everyday');
 const weekend = document.getElementById('weekend');
 const weekday = document.getElementById('weekday');
-
-//Ensuring only one of the three is selected at any one time
-everyday.addEventListener("click", function(event) {
-    if (everyday.checked) {
-        weekend.checked = false;
-        weekday.checked = false;
-    }
-});
-
-weekend.addEventListener("click", function(event) {
-    if (weekday.checked) {
-        weekday.checked = false;
-        everyday.checked = false;
-    }
-});
-
-weekday.addEventListener("click", function(event) {
-    if (weekday.checked) {
-        weekend.checked = false;
-        everyday.checked = false;
-    }
-});
