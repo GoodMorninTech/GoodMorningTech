975b7ca4c5a164901071270d1fa7df0d4c6d2db4
ImmaHarry <haritkhera@gmail.com>
2023-01-12 15:27:37 +0530

    Input box position fixed now & WIP responsive.

--- newspaper/static/JavaScript/landing-page/typewriter.js
+++ newspaper/static/JavaScript/landing-page/typewriter.js
@@ -2,8 +2,8 @@
      const typedTextSpan = document.querySelector("#typed-text");
      const cursorSpan = document.querySelector(".cursor");
 
-     const textArray = ["before work.", "during your morning coffee.", "while you commute to work.", "whenever you want!"]; /*EDIT THIS ARRAY FOR MORE TEXT*/
-     const typingDelay = 200;
+     const textArray = ["when you brew your coffee.", "when you start your day!", "while you commute to work.", "whenever you are not occupied."]; /*EDIT THIS ARRAY FOR MORE TEXT*/
+     const typingDelay = 175;
      const erasingDelay = 100;
      const newTextDelay = 2000; // Delay between current and next text
      let textArrayIndex = 0;
