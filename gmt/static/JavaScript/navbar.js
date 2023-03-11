7e70f5a5b2ebd1b3adff04f5c441b61f66d983d7
LevaniVashadze <vashadzelevani11@gmail.com>
2023-03-11 21:29:26 +0400

    navbar fix

--- gmt/static/JavaScript/navbar.js
+++ gmt/static/JavaScript/navbar.js
@@ -10,15 +10,16 @@ const heroSection = document.querySelector('#hero-section');
 
 document.addEventListener('DOMContentLoaded', () => {
     const navbarItem = document.getElementById(document.title);
+    // if the page is active, add the red border to the navbar item
     if (navbarItem) {
         navbarItem.classList.add('md:border-t-gmt-red-primary', 'md:border-b-0');
-        navbarItem.classList.remove('md:border-t-gmt-navbar-bg');
+        navbarItem.classList.remove('md:border-t-gmt-background');
     }
     else {
         const navigationItems = document.getElementById('navbar-item-list');
         for (let i = 0; i < navigationItems.children.length; i++) {
             navigationItems.children[i].children[0].classList.add('md:border-t-gmt-red-primary', 'md:border-b-0');
-            navigationItems.children[i].children[0].classList.remove('md:border-t-gmt-navbar-bg');
+            navigationItems.children[i].children[0].classList.remove('md:border-t-gmt-background');
         }
     }
 })
