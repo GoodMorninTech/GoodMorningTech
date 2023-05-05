fe6dc157882cac666a9fa075731aedbbb6f0f28d
LevaniVashadze <vashadzelevani11@gmail.com>
2023-05-06 00:08:39 +0400

    temporary fix

--- gmt/views/commands.py
+++ gmt/views/commands.py
@@ -21,6 +21,7 @@ from flask_mail import Message
 from markdown import markdown
 
 from .. import mail, mongo
+from ..extras import get_trending_repos, get_daily_coding_challenge
 from ..news import get_news
 from ..utils import random_language_greeting
 
@@ -124,13 +125,14 @@ def send_emails() -> None:
         random.shuffle(news)
         titles = [article["title"] for article in news]
 
-        html = render_template(
+        html = html = render_template(
             "general/news.html",
             posts=news,
             markdown=markdown,
             domain_name=current_app.config["DOMAIN_NAME"],
-            random_language_greeting=random_language_greeting(),
-        )
+            repos=get_trending_repos(),
+            coding_challenge=get_daily_coding_challenge(),
+            random_language_greeting=random_language_greeting())
         try:
             openai.api_key = current_app.config["OPENAI_API_KEY"]
             completion = openai.ChatCompletion.create(
