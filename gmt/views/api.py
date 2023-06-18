83978b8173278769376b5adf75bb5ef5e786f734
LevaniVashadze <vashadzelevani11@gmail.com>
2023-06-18 23:14:59 +0400

    add the backend for api key distribution

--- gmt/views/api.py
+++ gmt/views/api.py
@@ -1,10 +1,11 @@
 import datetime
 
 from bson import ObjectId
-from flask import Blueprint, Response, render_template, request
+from flask import Blueprint, Response, render_template, request, current_app
 from flask_login import current_user
+from flask_mail import Message
 
-from gmt import mongo
+from gmt import mongo, mail
 from gmt.utils import parse_json
 
 bp = Blueprint("api", __name__)
@@ -17,14 +18,34 @@ def api():
             {"_id": ObjectId(current_user.id)}
         )
 
-    return render_template("api/api.html")
+    if request.method == "POST":
+        user_email = request.form.get("email")
+
+        user = mongo.db.users.find_one({"email": user_email})
+        if not user:
+            return render_template("api/api.html", error="User not found")
+
+        msg = Message(
+            "Your API Key",
+            recipients=user_email,
+            sender=("Good Morning Tech", current_app.config["MAIL_USERNAME"]),
+            body=f"""The API key for your account is: {user["_id"]}
+            If you didn't request this, you can safely ignore this email.
+            """,
+        )
+        mail.send(msg)
+        return render_template("api/api.html", error=None, success=True)
+
+    return render_template("api/api.html", error=None)
 
 
 @bp.route("/api/news/")
 def news():
     api_key = request.headers.get("X-API-KEY")
-    user = mongo.db.users.find_one({"_id": ObjectId(api_key)})
+    if not api_key:
+        return Response(status=401)
 
+    user = mongo.db.users.find_one({"_id": ObjectId(api_key)})
     # if the user with that id isn't in the db, return 401
     if not user:
         return Response(status=401)
